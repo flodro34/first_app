7b78c7ba3e73e4eeddd390f59f9c705a8002b306
flodro34 <f.derosso@gmail.com>
2023-07-11 16:06:58 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:first_app/gradient_container.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          Color.fromARGB(255, 99, 100, 100),
+          Color.fromARGB(255, 13, 221, 211),
+        ),
+      ),
+    ),
+  );
+}
